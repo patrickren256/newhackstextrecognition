4ec43919250e07a911e4dfe959250ba698b61ae3
Brian Diep <brian.diep@mail.utoronto.ca>
2020-03-07 17:21:32 -0500

    added stuff

--- app/src/main/java/com/example/android/newhackstextrecognition/Storyboard.java
+++ app/src/main/java/com/example/android/newhackstextrecognition/Storyboard.java
@@ -1,9 +1,30 @@
 package com.example.android.newhackstextrecognition;
 
+import java.util.ArrayList;
+
 public class Storyboard {
 
-    private String sceneText;
+    private String scene_text;
 
-    public Storyboard() {
+    private Storyboard previous_scene;
+    private ArrayList<Storyboard> paths;
+
+    public Storyboard(String scene) {
+    }
+
+    public String getSceneText() {
+        return scene_text;
+    }
+
+    public Storyboard getPrevious_scene() {
+        return previous_scene;
+    }
+
+    public ArrayList<Storyboard> getPaths() {
+        return paths;
+    }
+
+    public void addPath(Storyboard newPath) {
+        this.paths.add(newPath);
     }
 }
